5b01ce9e905a6c873cbd73c7d4140ad2ae83b3f8
Valverde Daniel <valverdedaniel44@gmail.com>
2018-10-23 22:00:20 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,57 @@
+var request = require("request");
+
+// Store all of the arguments in an array
+var nodeArgv = process.argv;
+var command = process.argv[2];
+//movie or song
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+  if(i>3 && i<nodeArgv.length){
+    x = x + "+" + nodeArgv[i];
+  } else{
+    x = x + nodeArgv[i];
+  }
+}
+
+//switch case
+switch(command){
+
+  case "movie-this":
+    if(x){
+      omdbData(x)
+    } else{
+      omdbData("Mr. Nobody")
+    }
+  break;
+
+  default:
+    console.log("{Please enter a command:spotify-this-song, movie-this}");
+  break;
+}
+
+// Then run a request to the OMDB API with the movie specified
+
+function omdbData(movie){
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else {
+        console.log("error Occurred.")
+    }
+
+  )}
+;
